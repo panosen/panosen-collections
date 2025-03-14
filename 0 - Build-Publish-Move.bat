785d83be9c68e31e79e67e914df27a9ef5a7110b
harriszhang <harriszhang@live.cn>
2025-03-14 22:56:11 +0800

    0.6.4

--- 0 - Build-Publish-Move.bat
+++ 0 - Build-Publish-Move.bat
@@ -4,6 +4,8 @@ dotnet restore
 
 dotnet build --no-restore -c Release
 
+dotnet nuget push Panosen.Collections\bin\Release\Panosen.Collections.*.nupkg -s https://nuget.panosen.cn/v3/index.json -k 1cd8e026-9715-3c58-aa2c-42cd087c0b88 --skip-duplicate
+
 move /Y Panosen.Collections\bin\Release\Panosen.Collections.*.nupkg D:\LocalSavoryNuget\
 
 pause
